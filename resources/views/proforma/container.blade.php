1ca1ace76ca383507dba771ec1605d75a97b5744
phyzerbert <xian1017@outlook.com>
2019-09-15 03:26:27 +0800

    sale

--- resources/views/proforma/container.blade.php
+++ resources/views/proforma/container.blade.php
@@ -34,7 +34,6 @@
                 <div class="table-responsive px-1">
                     @php
                         $products = $invoice->items->pluck('product_id')->toArray();
-                        dd($products);
                         $footer_product_total = array();
                     @endphp
                     <table class="table table-bordered table-vcenter">

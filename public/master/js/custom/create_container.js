aa5c322e2e2b89aa6a400301aa40837d012be821
phyzerbert <xian1017@outlook.com>
2019-10-02 01:27:42 +0800

    fix add container

--- public/master/js/custom/create_container.js
+++ public/master/js/custom/create_container.js
@@ -36,7 +36,6 @@ var app = new Vue({
                     let proforma = response.data
                     this.week_c = proforma.week_c
                     this.week_d = proforma.week_d
-                    console.log(response.data)
                     if(response.data.shipment){
                         let shipment_items = response.data.shipment.items
                         for (let i = 0; i < shipment_items.length; i++) {
